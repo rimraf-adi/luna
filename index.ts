9c74aec4153f6e4ad82acf5f72889209a9694cae
Aditya Kinjawadekar <kinjawadekaradi112@gmail.com>
2024-10-25 23:27:20 +0530

    fix : add next and react routes

--- index.ts
+++ index.ts
@@ -1,31 +1,110 @@
 import * as fs from 'fs';
 import * as path from 'path'
+import { exec } from 'child_process';
+import { stdout } from 'process';
 
 const help = () => {
     console.log('hello! welcome to luna')
 }
-const init = (project: string) => {
-    const indexJS = path.join(project,'index.ts')
-    fs.mkdir(project, { recursive: true }, (err) => {
-        if (err) {
-            console.error('Error creating directory:', err);
-            return;
-        }
-        fs.writeFile(indexJS, '', 'utf8', (err) => {
+const initExpress = (projectName: string) => {
+    if (projectName != '.') {
+        fs.mkdir(projectName, (err) => {
             if (err) {
-                console.error('Error writing file:', err);
+                console.error(err);
                 return;
             }
+
+            fs.mkdir(path.join(projectName, 'src'), (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                fs.writeFile(path.join(projectName, 'src', 'index.ts'), '// this is the entry point of your project', 'utf8', (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+            })
+            fs.mkdir(path.join(projectName, 'lib'), (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                fs.writeFile(path.join(projectName, 'lib', 'lib.ts'), '// add controllers here', 'utf8', (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+            })
+
+
         });
-    });
+    }
+    else{
+       
+
+            fs.mkdir('src', (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                fs.writeFile(path.join('src', 'index.ts'), '// this is the entry point of your project', 'utf8', (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+            })
+            fs.mkdir(path.join('lib'), (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                fs.writeFile(path.join('lib', 'lib.ts'), '// add controllers here', 'utf8', (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                });
+            })
+
+
+
+
+    }
 
 }
+
+const initNext15 = (projectName : string) => {
+    exec(`npx create-next-app@latest ${projectName}`, (error: Error | null, stdout: string, stderr: string) => console.log(stdout))
+}
+
 function main() {
 
     const args = process.argv[2]
-    if (args == 'init') init('myProject1');
-    if (!args) { console.log('bolnaaa maahi bolnaaaa'); return }
-    console.log(args)
+
+    if (args == 'init')
+        if (process.argv[3] == 'express') initExpress(process.argv[4]);
+        if(process.argv[3] == 'next') initNext15(process.argv[4]);    
+    
+    
+    // if (!args) { help(); return }
+
+// exec('ls', (error: Error | null, stdout: string, stderr: string) => {
+//   if (error) {
+//     console.error(`error: ${error.message}`);
+//     return;
+//   }
+//   if (stderr) {
+//     console.error(`stderr: ${stderr}`);
+//     return;
+//   }
+//   console.log(stdout);   
+//   
+
+// });
 }
 
 
